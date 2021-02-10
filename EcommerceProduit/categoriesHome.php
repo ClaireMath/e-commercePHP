f090e2fc8eaee7b8bec4c9e183d7475120f28244
alexismajchrzak <alexis.majchrzak77@gmail.com>
2021-02-10 15:22:23 +0100

    ajout début des catégories

--- EcommerceProduit/categoriesHome.php
+++ EcommerceProduit/categoriesHome.php
@@ -7,43 +7,34 @@
 </head>
 <body>
 
-		
-
 <nav>
 
 	<ul>
-			<li id=""><a href="categoriesHome.php?categ=entree">Entrées </a>
+			<li id=""><a href="entree.php" >Entrées </a>
+
 				<ul class="sous">
-					<li><a >Entrées 1</a></li>
-					<li><a >Entrées 2</a></li>				
-                    <li><a>Entrées 3</a></li>
-					
+				
 				</ul>	
 			</li>
 
 			<li class=""><a href="#sport ">Plats </a>
+
 				<ul class="sous"> 
-					<li ><a href="" >Plats 1</a></li>
-					<li><a href=".html" >Plats 2</a></li>
-					<li><a href=".html" >Plats 3</a></li>
+					
 				</ul>	
 			</li>	
 			
 
 			<li class=""><a href="#mondeOuvert">Desserts </a>
+
 				<ul class="sous">
-					<li ><a href="" >Desserts 1 </a></li>
-					<li><a href=".html" >Desserts 2 </a></li>
-					<li><a href=".html" >Desserts 3 </a></li>
 
 				</ul>	
 			</li>
 
 			<li class=""><a href="#">Boissons</a>
+
 				<ul class="sous">
-					<li ><a href=".html">Boissons 1</a></li>
-					<li><a href=".html" >Boissons 2</a></li>
-					<li><a href=".html" >Boissons 3</a></li>
 				
 				</ul>	
 			</li>
