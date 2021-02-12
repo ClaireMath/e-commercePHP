d95875ae314d0ef0cbd61bb46c1348b5f061adcf
ClaireW <claira.m@live.fr>
2021-02-12 14:49:32 +0100

    mdp

--- db.php
+++ db.php
@@ -6,8 +6,8 @@ try {
     // pasw: Super-Groupe8
     // dbname: groupe8
     // port: 3306
-    // $bd = new PDO('mysql:host=127.0.0.1;port=3306;dbname=groupe8', 'root', '');
-    $bd = new PDO('mysql:host=127.0.0.1;port=8889;dbname=groupe8', 'root', 'root');
+    $bd = new PDO('mysql:host=127.0.0.1;port=3306;dbname=groupe8', 'root', '');
+    // $bd = new PDO('mysql:host=127.0.0.1;port=8889;dbname=groupe8', 'root', 'root');
     // $bd = new PDO('mysql:host=188.34.153.174;port=3306;dbname=groupe8', 'groupe8', 'Super-Groupe8');
 }
 catch (Exception $e) {
