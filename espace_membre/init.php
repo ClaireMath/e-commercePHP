c85b9c144c99d316078cad3ea44684a171780270
Nohan75 <nohanmarielouise@gmail.com>
2021-02-12 12:15:15 +0100

    Delete bad init file

--- espace_membre/init.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $bdd = new PDO('mysql:host=127.0.0.1;port=8889;dbname=espace_membre', 'root', 'root');
-    // $bdd = new PDO('mysql:host=127.0.0.1;port=3306;dbname=espace_membre', 'root', '');
-?>
